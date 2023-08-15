fdae3ea9fae0a15291c3c6cfc3eb1329378727e5
Chris Reeves <evschris@gmail.com>
2023-08-15 16:40:02 -0400

    adjust package script to clean local build directory

--- package.sh
+++ package.sh
@@ -1,6 +1,7 @@
+rm -r ./build/*
 deno run -A build-client.ts
-if [ ! -d build ]; then
-  mkdir build
+if [ ! -d ./build ]; then
+  mkdir ./build
 fi
 deno compile -A --unsafely-ignore-certificate-errors --target x86_64-unknown-linux-gnu -o build/pg-difficult_linux-x64 ./serve.ts
 deno compile -A --unsafely-ignore-certificate-errors --target x86_64-pc-windows-msvc -o build/pg-difficult_windows-x64 ./serve.ts
